9379ab2158991532c0f58cdb9f6fd33114ed4e33
Pallavinatraj <pallavinatraj31@gmail.com>
2025-05-29 12:26:54 +0530

    First Server

---
+++ First Node Server/app.js
@@ -0,0 +1,13 @@
+//Simple node.js Server
+const http = require('http')
+
+
+
+const server = http.createServer((req, res)=>{
+    console.log(req);
+});
+
+let PORT = 3000;
+server.listen(PORT, ()=>{
+  console.log(`The http://localhost/${PORT}`);
+});
